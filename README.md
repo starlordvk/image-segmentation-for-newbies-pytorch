030355933349046749f0dd2706670905f1703713
starlordvk <vkathuri@andrew.cmu.edu>
2022-06-26 09:51:50 -0700

    Prepare cityscapes datasets and datalaoder

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # image-segmentation-for-newbies-pytorch
 Semantic Image Segmentation from scratch in pytorch
+
+
+### Data Download
+
+Download leftImg8bit and gtFine data from Cityscapes website. Extract these 2 zips under one folder. 
---
+++ datasets/cityscapes.py
@@ -0,0 +1,33 @@
+import os
+import os.path as osp
+import numpy as np
+import math
+
+from tqdm import tqdm
+from PIL import Image
+from sklearn.metrics import roc_auc_score
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+import torchvision
+import torchvision.transforms as transforms
+
+
+# !!!!!!!! NO NEED TO WRITE CUSTOM DATASET CLASSES FOR CITY SCAPES. ALL HAIL PYTORCH !!!!!
+# Torch vision now supports city scapes and allows to easily load the cityscapes dataset
+# by just specifying the directory which has the leftImg8bit and gtFine folders.
+
+tfms = transforms.Compose([transforms.PILToTensor()])
+
+trainDataset = torchvision.datasets.Cityscapes('./', split='train', mode='fine',
+                     target_type='color', transform=tfms, target_transform=tfms)
+
+valDataset = torchvision.datasets.Cityscapes('./', split='val', mode='fine',
+                     target_type='color', transform=tfms, target_transform=tfms)
+
+testDataset = torchvision.datasets.Cityscapes('./', split='test', mode='fine',
+                     target_type='color', transform=tfms, target_transform=tfms)
